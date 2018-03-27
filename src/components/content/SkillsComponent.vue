b4156211aa294b65a71352d6ac74f0935ede97a9
Lex Martinez <lex0316@gmail.com>
2018-03-27 10:46:28 -0500

    minor fixes

--- src/components/content/SkillsComponent.vue
+++ src/components/content/SkillsComponent.vue
@@ -38,7 +38,7 @@
       </div>
 
       <div class="skill-container">
-        <p class="label"><span class="skill">React Native</span> <span class="pull-right level2">Semi-Pro</span></p>
+        <p class="label"><span class="skill">React, React Native</span> <span class="pull-right level2">Semi-Pro</span></p>
         <div class="progress">
           <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 61%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
